8f732f060f420d1de83a7aa14e08233fce2bd9d8
StreetCode Academy <streetcodeacademy@StreetCodes-MacBook-Pro.local>
2019-06-18 23:46:50 -0700

    Finished script to delete the final line

--- collect_data.py
+++ collect_data.py
@@ -33,27 +33,116 @@ def importData(f, list_of_files):
                 f.writelines(sentence + '\n')
                 f.writelines(word_to_analyze + '\n')
                 f.writelines(str(2) + '\n')
-                        
-        #f.writelines("-"*100 + '\n')  
 
+    
 #Will read through the list of files, format them appropriately, and write them to info.txt
 if  __name__ == "__main__":
-    f1 = open('./lstm-atae/airconditioner.txt','a')
+    f1 = open('./atae-lstm-cabasc/airconditioner.txt','w')
     curr_file = ['./data/AirConditioner.csv'] #'./data/ABS.csv', './data/Airbag.csv', './data/Ball-Joint.csv', './data/CabinAirFilter.csv', './data/MR-OIL.csv'
+    path = './atae-lstm-cabasc/airconditioner.txt'
     importData(f1, curr_file)
-    f2 = open("./lstm-atae/abs.txt", 'a')
+    
+    last_line = 0
+    with open(path, 'r') as f1:
+        content = f1.readlines()
+        last_line = len(content)
+    count = 1
+    with open(path, 'w') as f1:
+        for line in content:
+            if count == last_line:
+                f1.writelines(line.rstrip('\n'))
+            else:
+                f1.writelines(line)
+            count += 1
+
+    f2 = open("./atae-lstm-cabasc/abs.txt", 'w')
     curr_file = ['./data/ABS.csv']
+    path = "./atae-lstm-cabasc/abs.txt"
     importData(f2, curr_file)
-    f3 = open("./lstm-atae/airbag.txt", 'a')
+
+    last_line = 0
+    with open(path, 'r') as f2:
+        content = f2.readlines()
+        last_line = len(content)
+    count = 1
+    with open(path, 'w') as f2:
+        for line in content:
+            if count == last_line:
+                f2.writelines(line.rstrip('\n'))
+            else:
+                f2.writelines(line)
+            count += 1
+
+    f3 = open("./atae-lstm-cabasc/airbag.txt", 'w')
     curr_file = ['./data/Airbag.csv']
+    path = "./atae-lstm-cabasc/airbag.txt"
     importData(f3, curr_file)
-    f4 = open("./lstm-atae/balljoint.txt", 'a')
+
+    last_line = 0
+    with open(path, 'r') as f3:
+        content = f3.readlines()
+        last_line = len(content)
+    count = 1
+    with open(path, 'w') as f3:
+        for line in content:
+            if count == last_line:
+                f3.writelines(line.rstrip('\n'))
+            else:
+                f3.writelines(line)
+            count += 1
+
+    f4 = open("./atae-lstm-cabasc/balljoint.txt", 'w')
     curr_file = ['./data/Ball-Joint.csv']
+    path = "./atae-lstm-cabasc/balljoint.txt"
     importData(f4, curr_file)
-    f5 = open("./lstm-atae/cabinairfilter.txt", 'a')
+
+    last_line = 0
+    with open(path, 'r') as f4:
+        content = f4.readlines()
+        last_line = len(content)
+    count = 1
+    with open(path, 'w') as f4:
+        for line in content:
+            if count == last_line:
+                f4.writelines(line.rstrip('\n'))
+            else:
+                f4.writelines(line)
+            count += 1
+
+    f5 = open("./atae-lstm-cabasc/cabinairfilter.txt", 'w')
     curr_file = ['./data/CabinAirFilter.csv']
+    path = "./atae-lstm-cabasc/cabinairfilter.txt"
     importData(f5, curr_file)
-    f6 = open("./lstm-atae/oil.txt", 'a')
+
+    last_line = 0
+    with open(path, 'r') as f5:
+        content = f5.readlines()
+        last_line = len(content)
+    count = 1
+    with open(path, 'w') as f5:
+        for line in content:
+            if count == last_line:
+                f5.writelines(line.rstrip('\n'))
+            else:
+                f5.writelines(line)
+            count += 1
+
+
+    f6 = open("./atae-lstm-cabasc/oil.txt", 'w')
     curr_file = ['./data/MR-OIL.csv']
+    path = "./atae-lstm-cabasc/oil.txt"
     importData(f6, curr_file)
+
+    last_line = 0
+    with open(path, 'r') as f6:
+        content = f6.readlines()
+        last_line = len(content)
+    count = 1
+    with open(path, 'w') as f6:
+        for line in content:
+            if count == last_line:
+                f6.writelines(line.rstrip('\n'))
+            else:
+                f6.writelines(line)
+            count += 1
 
